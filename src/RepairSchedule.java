8c82c501db98b6129663aa649c4e5365c72c60b4
BT_1E10_20 <sample@bths.edu>
2022-02-09 12:00:24 -0500

    Initial commit

---
+++ src/RepairSchedule.java
@@ -0,0 +1,79 @@
+import java.util.ArrayList;
+
+public class RepairSchedule
+{
+    /** Each element represents a repair by an individual mechanic in a bay. */
+    private ArrayList<CarRepair> schedule;
+
+    /** Number of mechanics available in this schedule. */
+    private int numberOfMechanics;
+
+    /** Constructs a RepairSchedule object.
+     * Precondition: n >= 0
+     */
+    public RepairSchedule(int n)
+    {
+        schedule = new ArrayList<CarRepair>();
+        numberOfMechanics = n;
+    }
+
+    public ArrayList<CarRepair> getSchedule()
+    {
+        return schedule;
+    }
+
+    /** Attempts to schedule a repair by a given mechanic in a given bay as described in part (a).
+     * Precondition: 0 <= m < numberOfMechanics and b >= 0
+     */
+    public boolean addRepair(int m, int b)
+    {
+        boolean mech=true;
+        boolean ba=true;
+        for (int i =0; i<schedule.size();i++){
+            if (schedule.get(i).getMechanicNum()==m){
+                mech=false;
+            }
+        }
+        for (int i =0; i<schedule.size();i++){
+            if (schedule.get(i).getBayNum()==b){
+                ba=false;
+            }
+        }
+        if (mech&&ba) {
+            schedule.add((new CarRepair(m,b)));
+            return true;
+        }
+        else return false;
+    }
+
+    /** Returns an ArrayList containing the mechanic identifiers of all available mechanics,
+     * as described in part (b).
+     */
+    public ArrayList<Integer> availableMechanics()
+    {
+        ArrayList<Integer> mech = new ArrayList<Integer>();
+        String s ="";
+        for (int i =0; i< schedule.size();i++){
+            s+=schedule.get(i).getMechanicNum();
+        }
+        for (int i =0; i<schedule.size();i++){
+            if (s.indexOf(""+schedule.get(i).getMechanicNum())==-1) {
+                mech.add(schedule.get(i).getMechanicNum());
+            }
+        }
+        return mech;
+    }
+
+    /** Removes an element from schedule when a repair is complete. */
+    public void carOut(int b)
+    {
+        for (int i = 0; i < schedule.size(); i++)
+        {
+            CarRepair carAtIdx = schedule.get(i);
+            if (carAtIdx.getBayNum() == b)
+            {
+                schedule.remove(i);
+            }
+        }
+    }
+}
